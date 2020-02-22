a657f0adac51e0a3af6c19a044d9d50409157a7d
Brad Gibbs <bradgibbs@mac.com>
2020-02-22 14:33:30 -0600

    favicon?

--- src/gatsby-theme-apollo-docs/components/seo.js
+++ src/gatsby-theme-apollo-docs/components/seo.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import {Helmet} from 'react-helmet';
+import { Helmet } from 'react-helmet';
+import favicon from '../../images/favicon.ico';
 
 export default function SEO(props) {
   const {title, description, siteName, twitterCard, children, favicon} = props;
@@ -21,6 +22,7 @@ export default function SEO(props) {
     {/* Favicon stuff from realfavicongenerator.net */}
     <meta name="apple-mobile-web-app-title" content="thegyre.io" />
     <meta name="application-name" content="thegyre.io" />
+    <link rel="icon" href={favicon} />
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
